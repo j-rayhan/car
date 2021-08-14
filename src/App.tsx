8810a9468a8af39857faf94507712ad46aff5774
johir-rayhan <johir.rayhan01@gmail.com>
2021-08-14 11:05:42 +0600

    Test husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello 
+      <h1>Hello ....</h1>
 
-        
-        ....</h1>
-
-
-
-      <h1>Start your
-        
-         codding 2 ....</h1>
-
-
-
+      <h1>Start your codding 2 ....</h1>
     </div>
   )
-};
+}
 
 export default App

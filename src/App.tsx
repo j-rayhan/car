ff0cfe801d3401531863f7c6660d012d4aaa1c09
johir-rayhan <johir.rayhan01@gmail.com>
2021-08-14 10:39:16 +0600

    Test husky

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello ....</h1>
-      <h1>Start your codding  ....</h1>
+      <h1>Start your codding 2 ....</h1>
     </div>
   )
 }
